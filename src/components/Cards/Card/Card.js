709792bf80c0f74c7b942ece13950da0818ab6d9
theshaked <shaked2009@gmail.com>
2023-01-29 00:10:22 +0200

    Removed few unnecessary files from the project new cards and card components added - still WIP

---
+++ src/components/Cards/Card/Card.js
@@ -0,0 +1,17 @@
+import './Card.css'
+
+const Card = () => {
+    return <div className="card grow">
+        <img alt='robots' src='http://robohash.org/name?size=100x100'/>
+        <h2>name lastname</h2>
+        <line/>
+        <span>pizza prefrences
+        pizza prefrences
+        pizza prefrences
+        pizza prefrences
+        pizza prefrences
+        </span>
+    </div>
+};
+
+export default Card;
---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,17 @@
+import './Cards.css'
+ 
+import Card from "./Card/Card";
+
+const Cards = () => {
+    return <div className="cards">
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        </div>
+};
+
+export default Cards;
